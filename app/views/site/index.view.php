6de3beae5009f2c5d93474df9ee9ada554e48824
Gabriel.cunha <greis.reis.2003@gmail.com>
2025-10-18 19:33:15 -0300

    Praticamente termina o formulário

--- app/views/site/index.view.php
+++ app/views/site/index.view.php
@@ -10,13 +10,53 @@
     <div class="container">
         <form action="" method="post" class="formulario">
             <div class="caixaInput">
-                <div class="containerInput"><input type="text" name="name" id="" class="input"></div>
-                <div class="containerInput"><input type="email" name="email" class="input"></div>
+                <div class="containerInput">
+                    <input type="text" name="name" id="" class="input" required>
+                    <div class="labelLine">Digite seu nome</div>
+                </div>
+                <div class="containerInput">
+                    <input type="email" name="email" class="input" required>
+                    <div class="labelLine">Digite seu email</div>
+                </div>
             </div>
-            <div class="caixaInput"></div>
-            <div class="caixaInput"></div>
-            <div class="caixaInput"></div>
-            <div class="caixaInput"></div>
+            <div class="caixaInput">
+                <div class="containerInputs">
+                    <input type="date" class="input">
+                </div>
+                <div class="containerInputs">
+                    <input type="text" class="input" required>
+                    <div class="labelLine">Insira a descrição</div>
+                </div>
+                <div class="containerInputs">
+                    <input type="text" class="input" required>
+                    <div class="labelLine">Insira seu cpf</div>
+                </div>
+            </div>
+            <div class="caixaInput">
+                <div class="containerInput">
+                    <input type="text" class="input" required>
+                    <div class="labelLine">Digite seu telefone</div>
+                </div>
+                <div class="containerInput">
+                    <input type="text" class="input" required>
+                    <div class="labelLine">Digite seu RG</div>
+                </div>
+            </div>
+            <div class="caixaInputVertical">
+                <div class="containerInput vertical">
+                    <input type="password" class="input" required>
+                    <div class="labelLine">Digite sua senha</div>
+                </div>
+                <div class="containerInput vertical">
+                    <div class="containerInput vertical">
+                    <input type="password" class="input" required>
+                    <div class="labelLine">Confirme sua senha</div>
+                </div>
+                </div>
+            </div>
+            <div class="botaoSubmit">
+                <button type="submit"> CONFIRMAR</button>
+            </div>
         </form>
     </div>
 </body>
