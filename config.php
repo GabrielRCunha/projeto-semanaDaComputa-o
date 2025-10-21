72b07f383f492d23b87617ef7e2d739e3020af3a
Miguelcrf <migueldoliveira1@gmail.com>
2025-10-21 13:39:37 -0300

    config

--- config.php
+++ config.php
@@ -4,7 +4,7 @@ return [
     'database' => [
         'name' => 'db_semana_da_computacao',
         'username' => 'root',
-        'password' => '',
+        'password' => 'ice+',
         'connection' => 'mysql:host=127.0.0.1',
         'options' => [
             PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
